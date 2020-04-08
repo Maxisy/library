6fd4a4d4597a6dd4b242b3a02ae3657665a9814e
Maxisy <maks.sybicki@gmail.com>
2020-04-08 23:25:49 +0200

    adding another class

---
+++ src/Book.java
@@ -0,0 +1,8 @@
+public class Book {
+    String title;
+    String author;
+    int relaseDate;
+    int pages;
+    String publisher;
+    String isbn;
+}
--- src/Library.java
+++ src/Library.java
@@ -1,22 +1,23 @@
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.1";
+        final String appName = "Biblioteka v0.2";
 
-        String title = "W pustyni i w puszczy";
-        String author = "Henryk Sienkiewicz";
-        int relaseDate = 2010;
-        int pages = 296;
-        String publisher = "Greg";
-        String isbn = "9788373271890";
+        Book book1 = new Book();
+        book1.title = "W pustyni i w puszczy";
+        book1.author = "Henryk Sienkiewicz";
+        book1.relaseDate = 2010;
+        book1.pages = 296;
+        book1.publisher = "Greg";
+        book1.isbn = "9788373271890";
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
-        System.out.print(title + ", ");
-        System.out.print("Autor: " + author + ", ");
-        System.out.print("Data wydania: " + relaseDate + ", ");
-        System.out.print("Ilość stron: " + pages + ", ");
-        System.out.print("Wydawca: " + publisher + ", ");
-        System.out.println("ISBN: " + isbn);
+        System.out.print(book1.title + ", ");
+        System.out.print("Autor: " + book1.author + ", ");
+        System.out.print("Data wydania: " + book1.relaseDate + ", ");
+        System.out.print("Ilość stron: " + book1.pages + ", ");
+        System.out.print("Wydawnictwo: " + book1.publisher + ", ");
+        System.out.println("ISBN: " + book1.isbn);
 
     }
 }
