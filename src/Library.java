ee5bff7355ee310e641cfa577d3160ff51d21585
Maxisy <maks.sybicki@gmail.com>
2020-04-11 11:42:34 +0200

    adding methods and constructors

--- src/Library.java
+++ src/Library.java
@@ -1,23 +1,20 @@
-public class Library {
+class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.2";
+        final String appName = "Biblioteka v0.3";
 
-        Book book1 = new Book();
-        book1.title = "W pustyni i w puszczy";
-        book1.author = "Henryk Sienkiewicz";
-        book1.relaseDate = 2010;
-        book1.pages = 296;
-        book1.publisher = "Greg";
-        book1.isbn = "9788373271890";
+        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010
+                , 296, "Greg", "9788373271890");
+        Book book2  = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch",
+                2009, 352, "Helion", "9788324620845");
+        Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
+                "Bert Bates, Katherine Sierra", 2008, 851,
+                "McGraw-Hill Osborne Media", "9780071591065");
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
-        System.out.print(book1.title + ", ");
-        System.out.print("Autor: " + book1.author + ", ");
-        System.out.print("Data wydania: " + book1.relaseDate + ", ");
-        System.out.print("Ilość stron: " + book1.pages + ", ");
-        System.out.print("Wydawnictwo: " + book1.publisher + ", ");
-        System.out.println("ISBN: " + book1.isbn);
+        book1.printInfo();
+        book2.printInfo();
+        book3.printInfo();
 
     }
 }
