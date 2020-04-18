e0b524a09428c75dfb8a81f1bc2efedf41028be0
Maxisy <maks.sybicki@gmail.com>
2020-04-18 19:34:42 +0200

    adding user input

---
+++ src/pl/javastart/library/io/DataReader.java
@@ -0,0 +1,31 @@
+package pl.javastart.library.io;
+
+import pl.javastart.library.model.Book;
+
+import java.util.Scanner;
+
+public class DataReader {
+    private Scanner scanner = new Scanner(System.in);
+
+    public Book readAndCreateBook() {
+        System.out.println("Tytuł:");
+        String title = scanner.nextLine();
+        System.out.println("Autor:");
+        String author = scanner.nextLine();
+        System.out.println("Wydawnictwo:");
+        String publisher = scanner.nextLine();
+        System.out.println("ISBN:");
+        String isbn = scanner.nextLine();
+        System.out.println("Rok wydania:");
+        int releaseDate = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Ilość stron:");
+        int pages = scanner.nextInt();
+        scanner.nextLine();
+        return new Book(title, author, releaseDate, pages, publisher, isbn);
+    }
+
+    public void close(){
+        scanner.close();
+    }
+}
